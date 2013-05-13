bac32bc6b031b0f1dea321ba2423ea56a1101ccd
RawTech <RawTechX@gmail.com>
2013-05-13 12:12:20 +0100

    Update to code, added plugin.yml

--- RawTechNoPhysics/src/me/rawtech/nophysics/BlockListener.java
+++ RawTechNoPhysics/src/me/rawtech/nophysics/BlockListener.java
@@ -8,37 +8,33 @@ import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
 
-public class BlockListener implements Listener  {
-	
-	public static RawTechNoPhysics plugin;
+public class BlockListener implements Listener {
 
-	public BlockListener(RawTechNoPhysics instance)
-	{
-		plugin = instance;
-	}
-	
-	@EventHandler
-	public void onBlockSpread(BlockSpreadEvent event)
-	{
-        if (event.getNewState().getType() == Material.FIRE) event.setCancelled(true);
+    public static RawTechNoPhysics plugin;
+
+    public BlockListener(RawTechNoPhysics instance) {
+        plugin = instance;
     }
-	
-	@EventHandler
-	public void onBlockBurn(BlockBurnEvent event)
-	{
-		event.setCancelled(true);
-	}
-	
-	@EventHandler
-	public void onBlockFade(BlockFadeEvent event)
-	{
-		event.setCancelled(true);
-	}
-	
-	@EventHandler
-	public void onBlockGrow(BlockGrowEvent event)
-	{
-		event.setCancelled(true);
-	}
 
+    @EventHandler
+    public void onBlockSpread(BlockSpreadEvent event) {
+        if (event.getNewState().getType() == Material.FIRE) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBurn(BlockBurnEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockFade(BlockFadeEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockGrow(BlockGrowEvent event) {
+        event.setCancelled(true);
+    }
 }
--- RawTechNoPhysics/src/me/rawtech/nophysics/RawTechNoPhysics.java
+++ RawTechNoPhysics/src/me/rawtech/nophysics/RawTechNoPhysics.java
@@ -4,13 +4,12 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RawTechNoPhysics extends JavaPlugin {
-	
-	private BlockListener bListener = new BlockListener(this);
-	
-	public void onEnable()
-	{		
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvents(bListener, this);
-	}
 
+    private BlockListener bListener = new BlockListener(this);
+
+    @Override
+    public void onEnable() {
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents(bListener, this);
+    }
 }
